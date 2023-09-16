349af3f65aea694f0c5e019f0b5115e261d819c5
Brian Thorne <brian@hardbyte.nz>
2023-09-17 11:42:59 +1200

    Update pydantic

--- qabot/config.py
+++ qabot/config.py
@@ -1,14 +1,14 @@
 from typing import List, Optional
-
-from pydantic import BaseSettings
+from pydantic import AnyUrl
+from pydantic_settings import BaseSettings
 
 
 class Settings(BaseSettings):
     OPENAI_API_KEY: str
 
     QABOT_DATABASE_URI: Optional[str] = None
-    QABOT_CACHE_DATABASE_URI = "duckdb:///:memory:"
-    QABOT_MODEL_NAME = "gpt-4"
-    QABOT_TABLES: Optional[List[str]]
+    QABOT_CACHE_DATABASE_URI: AnyUrl = "duckdb:///:memory:"
+    QABOT_MODEL_NAME: str = "gpt-4"
+    QABOT_TABLES: Optional[List[str]] = None
     QABOT_ENABLE_WIKIDATA: bool = True
     QABOT_ENABLE_HUMAN_CLARIFICATION: bool = True
