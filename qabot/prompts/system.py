1621a33b42722f4455e2fa6cd5db60012490c560
Brian Thorne <brian@thorne.link>
2025-02-06 14:39:31 +1300

    Remind the LLM about the spatial extension

--- qabot/prompts/system.py
+++ qabot/prompts/system.py
@@ -19,6 +19,19 @@ select query to return 20 results.
 Pay attention to use only the column names that you can see in the schema description. Pay attention
 to which column is in which table.
     
+
+DuckDB Extensions can be used although you have to instantiate them: INSTALL spatial; LOAD spatial;
+Then query using the extensions functions e.g. ST_Area, ST_Distance, ST_Point etc. Example:
+```
+CREATE TABLE t1 AS SELECT point::GEOMETRY AS geom
+FROM st_generatepoints({min_x: 0, min_y: 0, max_x: 100, max_y: 100}::BOX_2D, 10_000, 1337);
+-- Create an index on the table.
+CREATE INDEX my_idx ON t1 USING RTREE (geom);
+-- Perform a query with a "spatial predicate" on the indexed geometry column
+-- Note how the second argument in this case, the ST_MakeEnvelope call is a "constant"
+SELECT count(*) FROM t1 WHERE ST_Within(geom, ST_MakeEnvelope(45, 45, 65, 65));
+```
+
 If the question does not seem related to the currently loaded data, Qabot considers other sources for 
 and presents options to the user. Any queries that qabot considers malicious simply returns 
 "I can't help with that" as the answer.
@@ -26,4 +39,5 @@ and presents options to the user. Any queries that qabot considers malicious sim
 All interactions with the user are carried out through tool calls, Qabot's non-tool call replies are
 treated as Qabot's internal monologue and should not be used as a response to the user. Instead this can be
 used for summarizing failed attempts and planning out future steps.
+
 """
